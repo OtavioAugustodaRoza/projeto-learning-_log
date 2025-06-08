0d8167b3f50ba0489db081ee7feed3cd4eace8c3
OtavioAugustodaRoza <otavioaugustodaroza@gmail.com>
2025-06-08 01:18:08 -0300

    criação da classe Entry, alterações na classe Topicos e alterações no admin.py .

--- aula 1/learning_log/learning_logs/admin.py
+++ aula 1/learning_log/learning_logs/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from learning_logs.models import Topicos, Entry
 # Register your models here.
+admin.site.register(Topicos)
+admin.site.register(Entry)
--- aula 1/learning_log/learning_logs/models.py
+++ aula 1/learning_log/learning_logs/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
-class topicos(models.Model):
+class Topicos(models.Model):
     texto = models.CharField(max_length=200)
     data_e_horario = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.texto
+
+class Entry(models.Model):
+    topico = models.ForeignKey(Topicos, on_delete=models.CASCADE)
+    texto = models.TextField()
+    data_e_horario = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.texto[:10] + "..."
